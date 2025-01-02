61085aea4d051790c906ed1ef9d2918986bc7527
VijayVarma26 <varmavijay2021@gmail.com>
2025-01-02 20:11:04 +0530

    Added explanation comments

--- stack/Stack.py
+++ stack/Stack.py
@@ -1,4 +1,4 @@
-# Stack Class with required Functions
+# Stack Implementation Code 
 class Stack():
     def __init__(self):
         self.items = []
@@ -16,21 +16,24 @@ class Stack():
         return self.items[-1]
     
 
+# Testing Stack Implementation
 
-# Testing
+# Creating Stack Object
 s = Stack()
 
+# Checking if stack is empty
 print(s.isEmpty())
 
+# Pushing Elements in the Stack
 s.push("Vijay")
 s.push(1)
 s.push(2)
 s.push(True)
 
+# Poping elements from stack
 print(s.pop())
 
-print(s.isEmpty())
-
+# Checking the top element of the stack 
 print(s.peek())
 
 
