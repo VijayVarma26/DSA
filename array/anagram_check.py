46d23482224081062be2b8246dfff282814181ed
Vijay Varma <vijay.varma@iongroup.com>
2025-01-03 20:29:07 +0530

    Added unit tests in anagram check

--- array/anagram_check.py
+++ array/anagram_check.py
@@ -1,3 +1,5 @@
+import unittest
+
 # Two strings are anagram of each other if 
 
 # This implementation uses sorting and uses the sorted strings to check if they are equal
@@ -16,8 +18,8 @@ def check_anagram_using_sorting(str1, str2):
 
 # This implementation uses counting the frequency of each character in the string
 def check_anagram_using_counting(str1, str2):
-    str1 = str1.replace(" ", "")
-    str2 = str2.replace(" ", "")
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
 
     if len(str1) != len(str2):
         return False
@@ -39,4 +41,23 @@ def check_anagram_using_counting(str1, str2):
         if seen[key] != 0:
             return False
 
-    return True        
+    return True
+
+class TestAnagramCheck(unittest.TestCase):
+
+    def test_check_anagram_using_sorting(self):
+        self.assertTrue(check_anagram_using_sorting("listen", "silent"))
+        self.assertTrue(check_anagram_using_sorting("triangle", "integral"))
+        self.assertFalse(check_anagram_using_sorting("apple", "pale"))
+        self.assertTrue(check_anagram_using_sorting("A gentleman", "Elegant man"))
+        self.assertFalse(check_anagram_using_sorting("Hell1", "Olelh"))
+
+    def test_check_anagram_using_counting(self):
+        self.assertTrue(check_anagram_using_counting("listen", "silent"))
+        self.assertTrue(check_anagram_using_counting("triangle", "integral"))
+        self.assertFalse(check_anagram_using_counting("apple", "pale"))
+        self.assertTrue(check_anagram_using_counting("A gentleman", "Elegant man"))
+        self.assertFalse(check_anagram_using_counting("Hell1", "Olelh"))
+
+if __name__ == '__main__':
+    unittest.main()
