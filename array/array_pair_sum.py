f31d993f262cae31f331d49c13b5601d8270e13e
Vijay Varma <vijay.varma@iongroup.com>
2025-01-04 13:34:57 +0530

    Added Test cases in array pair sum problem

--- array/array_pair_sum.py
+++ array/array_pair_sum.py
@@ -1,16 +1,27 @@
+import unittest
+
 def array_pair_sum(arr, target):
     seen = set()
     output = set()
     for i in arr:
-        diff = target-i
+        diff = target - i
         if diff in seen:
             output.add((min(diff, i), max(diff, i)))
         else:
             seen.add(i)
 
-    if seen:
+    if output:
         return output
-    return None  
+    return None
 
-print(array_pair_sum([1, 2, 3, 4, 5], 6))              
+class TestArrayPairSum(unittest.TestCase):
 
+    def test_array_pair_sum(self):
+        self.assertEqual(array_pair_sum([1, 2, 3, 4, 5], 6), {(1, 5), (2, 4)})
+        self.assertEqual(array_pair_sum([1, 2, 3, 4, 5], 10), None)
+        self.assertEqual(array_pair_sum([1, 2, 3, 4, 5, 5], 10), {(5, 5)})
+        self.assertEqual(array_pair_sum([1, 2, 3, 4, 5], 7), {(2, 5), (3, 4)})
+        self.assertEqual(array_pair_sum([], 6), None)
+
+if __name__ == '__main__':
+    unittest.main()
